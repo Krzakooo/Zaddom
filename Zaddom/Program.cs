53be66affae49d410313c276ee620bd1a167a83d
Krzakooo <55135961+Krzakooo@users.noreply.github.com>
2021-10-23 14:22:32 +0200

    Add project files.

---
+++ Zaddom/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Zaddom
+{
+    class Program
+    {
+
+        static void prime(int n)
+        {
+            for (int p = 2; p < n; p++)
+            {
+                bool check = false;
+                for (int i = 2; i < p; i++)
+                {
+                    if (p % i == 0)
+                    {
+                        check = true;
+                    }
+                }
+                if (check == false)
+                {
+                    Console.WriteLine(p);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Podaj liczbę: ");
+            int input = int.Parse(Console.ReadLine());
+            prime(input);
+
+        }
+    }
+}
